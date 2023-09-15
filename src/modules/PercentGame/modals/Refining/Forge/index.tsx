aa30e948b7626eadf80f29e2c021d206eae8084b
Attou09 <nmphat0912@gmail.com>
2023-09-15 23:56:12 +0700

    Add alert comming soon when click  merge button

--- src/modules/PercentGame/modals/Refining/Forge/index.tsx
+++ src/modules/PercentGame/modals/Refining/Forge/index.tsx
@@ -39,7 +39,7 @@ const Forge = ({ iconSize = 40, gap = 2 }: Props) => {
 
   const onMerge = () => {
     if (loading) return
-    // setLoading(true)
+    alert('Comming soon ^^!!')
   }
 
   return (
@@ -83,10 +83,13 @@ const Forge = ({ iconSize = 40, gap = 2 }: Props) => {
             fontSize: 20,
             fontWeight: 'bold',
             height: 60,
+            border: 'none',
             borderRadius: 40,
             background: !disabled
               ? 'linear-gradient(to right, var(--color-primary), var(--color-secondary))'
               : 'rgba(0, 0, 0, 0.45)',
+            boxShadow:
+              'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
           }}
           block
           onClick={onMerge}
