d0d25cbb2e27f60b91056f3dd383588fff47a0a4
Attou09 <nmphat0912@gmail.com>
2023-09-16 16:32:39 +0700

    Add button merge action

--- src/modules/PercentGame/components/Header/Bag.tsx
+++ src/modules/PercentGame/components/Header/Bag.tsx
@@ -3,33 +3,37 @@ import { Button } from 'antd'
 import modal from 'modules/modal/provider'
 import { Backpack } from 'components/Icons/Game'
 
+const btnStyleDefault = {}
+
 interface Props {
   rounded?: boolean
+  iconSize?: number
+  btnStyle?: { [key: string]: string | number }
+  shape?: 'circle' | 'round' | 'default'
 }
 
-const Bag = ({ rounded = false }: Props) => {
+const Bag = ({
+  rounded = false,
+  iconSize = 20,
+  btnStyle = btnStyleDefault,
+  shape = 'default',
+}: Props) => {
   const onClick = () => {
     modal.percentGame.openBag()
   }
   return (
-    <div
-      className="consume-item bag"
+    <Button
+      type="primary"
+      shape={rounded ? 'circle' : shape}
+      icon={<Backpack size={iconSize} classNames="my-bag-icon" />}
+      className="btn-item-action btn-open-bag size--medium"
+      onClick={onClick}
       style={{
-        marginRight: rounded ? 10 : 0,
+        width: rounded ? 'auto' : '100%',
+        borderRadius: rounded ? '50%' : 20,
+        ...btnStyle,
       }}
-    >
-      <Button
-        type="primary"
-        shape="circle"
-        icon={<Backpack width={20} height={20} classNames="my-bag-icon" />}
-        className="my-bag-btn btn-item-action"
-        onClick={onClick}
-        style={{
-          width: rounded ? 'auto' : '100%',
-          borderRadius: rounded ? '50%' : '4px',
-        }}
-      />
-    </div>
+    />
   )
 }
 
--- src/modules/PercentGame/components/Menu/index.tsx
+++ src/modules/PercentGame/components/Menu/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { Button } from 'antd'
+import modal from 'modules/modal/provider'
+import { ScrollUnfurled } from 'components/Icons/Game'
 import Bag from '../Header/Bag'
 import './style.scss'
 
 const Menu = () => {
+  const onClickOpenMerge = () => {
+    modal.percentGame.refining()
+  }
   return (
     <div className="game-menu-area section-border">
-      <Bag />
+      <Bag iconSize={28} />
+      <Button
+        type="primary"
+        icon={<ScrollUnfurled size={28} classNames="my-bag-icon" />}
+        className="btn-item-action btn-open-bag size--medium"
+        onClick={onClickOpenMerge}
+        style={{
+          width: '100%',
+          marginTop: 10,
+          borderRadius: 20,
+        }}
+      />
     </div>
   )
 }
